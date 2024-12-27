43adbf345f9256b1d872a3178babdc4aa5749eda
ali bonaftizadeh <bonaftizadeh@yektanet.ir>
2024-12-27 17:15:20 +0330

    Fix run bugs.

--- src/main/java/config/SwaggerConfig.java
+++ src/main/java/config/SwaggerConfig.java
@@ -1,33 +1,21 @@
 package config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.yourpackage.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Form Management API")
-                .description("API documentation for managing forms and fields")
-                .version("1.0.0")
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Form Management API")
+                        .description("API documentation for managing forms and fields")
+                        .version("1.0.0"));
     }
 }
 
